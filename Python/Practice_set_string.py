323ef3eafe39af62bb46af8d2eb5d91b1e4fd22c
AmmaraAkhtar <1144ammara@gmail.com>
2025-01-18 15:56:20 +0500

    string complete

--- Python/Practice_set_string.py
+++ Python/Practice_set_string.py
@@ -1,2 +1,26 @@
-name=input("Enter the name:")
-print("GOOD AFTERNOON",name)
+# name=input("Enter the name:")
+# print("GOOD AFTERNOON",name)
+
+# name=input("Enter name:")
+# Data=input("Enter Data:")
+# print(f"Dear <|{name}|>,you are selected!<|{Data}|>")
+
+
+# string="Ammara  Akhtar"
+# print(string.find("  "))
+# print(string.replace("  "," "))
+
+
+# Escape Sequence
+
+# letter="Dear Harry, this python Course is nice. Thanks!"
+# print(letter)
+
+
+
+letter= ''' 
+Dear <|name|>,
+             you are selected!
+                      <|Data|>
+'''
+print(letter.replace("<|name|>","Ammara").replace("<|Data|>","19, jan 2025"))
