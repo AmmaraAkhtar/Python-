89c7d1be9a8b51fbe92f6a5ea328b66ab3161887
AmmaraAkhtar <1144ammara@gmail.com>
2025-01-18 16:20:33 +0500

    tuple/list complete

---
+++ Python/List.py
@@ -0,0 +1,8 @@
+list=[1,2,3,4,11,0,5]
+print(list.reverse())
+print(list)
+
+
+list=[1,2,3,4,11,0,5]
+print(list.sort())
+print(list)
---
+++ Python/list_tuple_practiceSet.py
@@ -0,0 +1,22 @@
+# list1=[]
+# for i in range(1,8):
+#     ls=input("Enter the Fruit:")
+#     list1.append(ls)
+# print(list1)
+
+# list=[40,80,20,65,23]
+# list.sort()
+# print(list)
+
+# tuple=(1,2,3)
+# tuple=tuple(1,2,3,4)
+# print(tuple)
+
+
+# tuple=(7,0,8,0,0,1)
+# print(tuple.count(0))
+
+
+
+list=[1,2,3,4]
+print(sum(list))
